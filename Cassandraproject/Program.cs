cb66844449b19b3dcdb878bd892bfddab93ed235
Arafat-Zaman <arafat.zaman.anik@gmail.com>
2020-10-15 01:57:25 +0600

    Add project files.

---
+++ Cassandraproject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Cassandra;
+
+namespace Cassandraproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cluster = Cluster.Builder()
+                .AddContactPoint("127.0.0.1")
+                .Build();
+
+            var session = cluster.Connect("test_keyspace");
+
+            session.Execute("INSERT INTO test_table(id, first_name, last_name) VALUES (uuid(),'Jim','kary')");
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
